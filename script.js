d1e98599df37d6baae757030366b05390df96af5
alec <abacqu0716@gmail.com>
2022-05-10 13:03:08 -0500

    updated all files

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const URL = "https://api.teleport.org/api/";
 
 // ELEMENT REFERENCES
 const $city = $('#city');
-const $publictransport = $('#publictransport');
+const $publictransport = $('#publictransit');
 const $lunch = $('#lunch');
+const $housing = $('#housing');
+const $costofliving = $('#costofliving');
 const $form = $('form');
 const $input = $('input[type="text"]');
 
 
 // EVENT LISTENERS
 
-$form.on('submit',getCityId)
+$form.on('submit', getCityId)
 
 // FUNCTIONS
 
@@ -18,37 +20,45 @@ function getCityId(event) {
     event.preventDefault()
     const userInput = $input.val();
 
-    $.ajax(URL + 'cities/?search=' + userInput).then(function(data) {
+    $.ajax(URL + 'cities/?search=' + userInput).then(function (data) {
         console.log('city data is ready ' + data);
-        console.log(data._embedded["city:search-results"]);
+        let hrefGeoId = data._embedded["city:search-results"][0]._links["city:item"].href
+        let locateCity = hrefGeoId.split('/');
+        let getGeoCityId = locateCity[locateCity.length - 2];
+        getUrbanSlug(getGeoCityId);
     })
-    
+
 }
 
+function getUrbanSlug(getGeoCityId) {
+    $.ajax(URL + 'cities/' + getGeoCityId).then(function (data) {
+        console.log(data._links["city:urban_area"].href);
+        let locateUrbanHref = data._links["city:urban_area"].href;
+        let SplitUA = locateUrbanHref.split('/')
+        let urbanSlug = SplitUA[SplitUA.length - 2];
+        getUrbanDetails(urbanSlug);
+        getUrbanScores(urbanSlug);
+        getImage(urbanSlug);
+    });
+}
 
+function getUrbanDetails(getUrbanSlug) {
+    $.ajax(URL + `urban_areas/${getUrbanSlug}/details/`).then(function (data) {
+        $publictransport.text(data.categories[3].data[7].currency_dollar_value);
+        $lunch.text(data.categories[3].data[8].currency_dollar_value);
+    })
+}
 
+function getUrbanScores(getUrbanSlug) {
+    $.ajax(URL + `urban_areas/${getUrbanSlug}/scores/`).then(function (data) {
+        $housing.text(data.categories[0].score_out_of_10);
+        $costofliving.text(data.categories[1].score_out_of_10);
+    })
+}
 
-
-
-
-
-
-// function handleGetData(event){
-
-//     event.preventDefault()
-//     const userInput = $input.val();
-
-//     $.ajax(URL + userInput).then(function(data) {
-//         console.log('city data is ready')
-//         console.log(data)
-//         //returning data from city name
-//         $city.text(data.City)
-//         $publictransport.text(data.Publictransport)
-//         $lunch.text(data.Lunch)
-//         $('main').append(`<img src="${data.Poster}"/>`)
-//     }, function(error) {
-//         console.log('something is wrong')
-//         console.log(error)
-//     })
-
-// }
+function getImage(getUrbanSlug) {
+    $.ajax(URL + `urban_areas/${getUrbanSlug}/images/`).then(function (data) {
+        // console.log(data.photos[0].image["web"]);
+        $('main').append(`<img src="${data.photos[0].image["web"]}"/>`)
+    })
+}
