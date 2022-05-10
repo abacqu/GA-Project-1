072f5670186ec61d29917a1fd0ea8789deccd1af
alec <abacqu0716@gmail.com>
2022-05-10 18:54:48 -0500

    updated files with including new css

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ const $lunch = $('#lunch');
 const $housing = $('#housing');
 const $costofliving = $('#costofliving');
 const $form = $('form');
+const $img = $('<img>');
 const $input = $('input[type="text"]');
+const $main = $('main');
+
+// const $cityInfo = $('<div>');
+// $cityInfo.addClass('cityInfo');
+// $main.append($cityInfo);
 
 
 // EVENT LISTENERS
@@ -21,7 +27,9 @@ function getCityId(event) {
     const userInput = $input.val();
 
     $.ajax(URL + 'cities/?search=' + userInput).then(function (data) {
-        console.log('city data is ready ' + data);
+        console.log( data);
+        $city.hide().fadeIn(1000);
+        $city.text(data._embedded["city:search-results"][0].matching_full_name);
         let hrefGeoId = data._embedded["city:search-results"][0]._links["city:item"].href
         let locateCity = hrefGeoId.split('/');
         let getGeoCityId = locateCity[locateCity.length - 2];
@@ -44,13 +52,19 @@ function getUrbanSlug(getGeoCityId) {
 
 function getUrbanDetails(getUrbanSlug) {
     $.ajax(URL + `urban_areas/${getUrbanSlug}/details/`).then(function (data) {
+
+        console.log(data.categories[3].data[7].currency_dollar_value);
+        $publictransport.hide().fadeIn(1000);
         $publictransport.text(data.categories[3].data[7].currency_dollar_value);
+        $lunch.hide().fadeIn(1000);
         $lunch.text(data.categories[3].data[8].currency_dollar_value);
     })
 }
 
 function getUrbanScores(getUrbanSlug) {
     $.ajax(URL + `urban_areas/${getUrbanSlug}/scores/`).then(function (data) {
+        $housing.hide().fadeIn(1000);
+        $costofliving.hide().fadeIn(1000);
         $housing.text(data.categories[0].score_out_of_10);
         $costofliving.text(data.categories[1].score_out_of_10);
     })
@@ -59,6 +73,10 @@ function getUrbanScores(getUrbanSlug) {
 function getImage(getUrbanSlug) {
     $.ajax(URL + `urban_areas/${getUrbanSlug}/images/`).then(function (data) {
         // console.log(data.photos[0].image["web"]);
-        $('main').append(`<img src="${data.photos[0].image["web"]}"/>`)
+        $img.hide().fadeIn(1000);
+        $img.attr('src', data.photos[0].image["web"]);
+        $img.width('70%');
+        $img.height('auto');
+        $main.append($img);
     })
 }
